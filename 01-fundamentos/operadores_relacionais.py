69bd670a532f5509d1832793635924606f887045
Maykon Freitas <maykon17programmer@gmail.com>
2022-10-26 15:29:17 -0300

    feat: adiciona o arquivo desafio_operadores_relacionais.py

---
+++ 01-fundamentos/operadores_relacionais.py
@@ -0,0 +1,17 @@
+# Maior que ... ( > )
+print(3 > 2)
+
+# Menor que ... ( < )
+print(3 < 2)
+
+# Maior ou igual que ... ( >= )
+print(3 >= 2)
+
+# Menor ou igual que ... ( <= )
+print(3 <= 2)
+
+# Diferente que ... ( != )
+print(3 != 2)
+
+# É igual a ... (==)
+print(3 == 2)

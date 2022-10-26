05ed5d5bc276f4b41d15bf51e1b4420e1ddea086
Maykon Freitas <maykon17programmer@gmail.com>
2022-10-26 14:58:31 -0300

    feat: adiciona o arquivo tipos_basicos.py

---
+++ 01-fundamentos/tipos_basicos.py
@@ -0,0 +1,21 @@
+# Tipos básicos em Python
+
+# String -> "ISSO É UM STRING"
+nome = "Maykon"
+
+# Booleano -> True ou False
+solteiro = True
+namorando = False
+
+# Númericos -> Inteiros e Decimais/Float
+idade = 20 # Inteiro
+altura = 1.65 # Decimal
+
+# Array/listas -> [..., ..., ...]
+notas = [10, 8, 9, 10]
+
+# Dicionarios -> { chave1: valor1, chave2: valor2 }
+dicionario = { "nome": "Azir", "level": 20, "classe": "Mago" }
+
+# None -> Nulo || Null
+dano = None

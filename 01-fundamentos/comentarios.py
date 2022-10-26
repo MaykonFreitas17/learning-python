49a86d8e11f129054bf191a32692523f8c8e032b
Maykon Freitas <maykon17programmer@gmail.com>
2022-10-26 15:10:43 -0300

    feat: adiciona o arquivo comentarios.py

---
+++ 01-fundamentos/comentarios.py
@@ -0,0 +1,13 @@
+# isso é um comentario - essa linha não afeta a execução do cógido
+
+# varivel nome
+nome = "Azir"
+
+# comentario de mais de uma linha
+
+"""
+  Isso é
+  um comentario
+  de mais de uma linha
+"""
+

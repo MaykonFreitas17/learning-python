2a0ecf709b2255b65bf89e663a2662b4d4b134e1
Maykon Freitas <maykon17programmer@gmail.com>
2022-10-26 15:17:56 -0300

    feat: adiciona o arquivo operadores_aritmeticos.py

---
+++ 01-fundamentos/operadores_aritmeticos.py
@@ -0,0 +1,21 @@
+# Soma ( + )
+print(1 + 1)
+
+# Subtração ( - )
+print(1 - 1)
+
+# Multiplicação ( * )
+print(1 * 1)
+
+# Divisão Decimal ( / )
+print(1 / 1)
+
+# Divisão inteira (//)
+print(3 // 2)
+
+# Potência (**)
+print(2 ** 2)
+
+# Resto da Divisão
+print(2 % 2) # 0
+print(3 % 2) # 1

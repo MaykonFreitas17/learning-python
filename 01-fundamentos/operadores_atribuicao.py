b5a60a0eb23e8cafd6534e93938e5e7db6ad5a68
Maykon Freitas <maykon17programmer@gmail.com>
2022-10-26 15:37:33 -0300

    feat: adiciona o arquivo operadores_atribuicao.py

---
+++ 01-fundamentos/operadores_atribuicao.py
@@ -0,0 +1,30 @@
+# Recebe ...
+a = 2 # "a" recebe o valor de 2
+
+# atribuição aditiva
+a = a + 1 
+a += 1 # outra maneira de fazer a atribuição de cima
+
+# atribuição subtrativa
+a = a - 1 
+a -= 1 # outra maneira de fazer a atribuição de cima
+
+# atribuição multiplicativa
+a = a * 1 
+a *= 1 # outra maneira de fazer a atribuição de cima
+
+# atribuição divisiva
+a = a / 1 
+a /= 1 # outra maneira de fazer a atribuição de cima
+
+# atribuição modular/modulativa
+a = a % 1 
+a %= 1 # outra maneira de fazer a atribuição de cima
+
+# atribuição com potência
+a = a ** 1 
+a **= 1 # outra maneira de fazer a atribuição de cima
+
+# atribuição divisiva inteira
+a = a // 1 
+a //= 1 # outra maneira de fazer a atribuição de cima

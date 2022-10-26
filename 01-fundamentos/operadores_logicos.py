f76a936d3e19abbe0094b60c537545a4536f435b
Maykon Freitas <maykon17programmer@gmail.com>
2022-10-26 15:50:42 -0300

    feat: adiciona o arquivo operadores_logicos.py

---
+++ 01-fundamentos/operadores_logicos.py
@@ -0,0 +1,16 @@
+# OU - or
+print(True or False)
+
+# E - And
+print(True and False)
+
+# OU EXCLUSIVO - XOR
+print(True != False)
+
+# NEGAÇÃO (unário) - NOT
+print(not False) # -> True
+
+# Operadores Bit a Bit
+print(True & False) # AND
+print(False | True) # OR
+print(True ^ False) # XOR

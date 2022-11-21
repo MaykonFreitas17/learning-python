2380c5e2d75b509dedb993f935a9bc668a88e96c
Maykon Freitas <maykon17programmer@gmail.com>
2022-11-21 15:57:16 -0300

    feat: adiciona o arquivo dict.py

---
+++ 04-estrutura-de-dados/dict.py
@@ -0,0 +1,20 @@
+# Dicts ou Dicionários (Objetos em outras linguagens)
+
+# Sintaxe => nome_dict = { key: valor, key2: valor2 }
+notas = {
+  "maykon": [10, 10, 10, 10],
+  "bruno": [8, 8, 8, 8],
+}
+
+# Acessando os valores do dicionário
+print(notas["maykon"])
+print(notas["bruno"])
+
+# Dicionários com chaves númericos(inteiros)
+weekdays = {
+  1: "Domingo",
+  2: "Segunda-Feira",
+  3: "Terça-Feira",
+}
+
+print(weekdays[1], weekdays[2], weekdays[3])

b244b2cfa0eaceae97bd2121895433c1e357bf2d
Maykon Freitas <maykon17programmer@gmail.com>
2022-11-22 10:54:48 -0300

    feat: adiciona o arquivo heranca.py

---
+++ 06-orientacao-a-objeto/heranca.py
@@ -0,0 +1,33 @@
+class Pessoa:
+  def __init__(self, nome, idade, cpf):
+    self.nome = nome
+    self.idade = idade
+    self.cpf = cpf
+  
+  def show_informations(self):
+    print('==========Infors==========')
+    print(f'nome: {self.nome}')
+    print(f'idade: {self.idade}')
+    print(f'cpf: {self.cpf}')
+    print('==========================')
+
+# a classe Estudante está herdando todos os atributos e metodos da classe Pai (Pessoa)
+class Estudante(Pessoa):
+  # Sobrescrevendo o metodo construtor do pai
+  def __init__(self, nome, idade, cpf, curso):
+    super().__init__(nome, idade, cpf)
+    self.curso = curso
+
+  def show_informations(self):
+    print('==========Infors==========')
+    print(f'nome: {self.nome}')
+    print(f'idade: {self.idade}')
+    print(f'cpf: {self.cpf}')
+    print(f'curso: {self.curso}')
+    print('==========================')
+
+p1 = Pessoa('Maykon Freitas', 20, "000.000.000-00")
+p1.show_informations()
+
+e1 = Estudante('Bruno Sousa', 18, "000.000.000-00", "Ciência da Computação")
+e1.show_informations()
